4a3cd6327f95ed35410138f0d6f28a677fd64244
maoshuai <zhucemid@gmail.com>
2019-07-07 16:05:05 +0800

    update to .source

--- test/test.sh
+++ test/test.sh
@@ -1,20 +1,8 @@
-##################################
-# 简单shell日志工具 测试
-# author maoshuai  http://imshuai.com
-# 
-##################################
+. simpleLog4sh.source
 
-# 导入simplelog4sh.sh
-. ../src/simplelog4sh.sh
-
-# 调用方法
-logInfo hello, world
-logDebug "hello, logDebug"
-logInfo "hello, logInfo"
-logWarn "hello, logWarn"
-logError "hello, logError"
-
-myEcho "hello, myEcho"
-myEchoError "hello, myEchoError"
-
-throw "Arguments Number Error"
+myEcho "helll"
+myEchoError "myEchoError"
+logInfo "logInfo"
+logDebug "logDebug"
+logError "logError"
+logWarn "logWarn"
